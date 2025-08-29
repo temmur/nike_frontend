c66144ca58e817ecac2782db842c6aef591d15cc
Daler <dalerwresling2011@gmail.com>
2025-08-29 14:40:27 +0500

    banner bottom created

--- src/views/PIndex.vue
+++ src/views/PIndex.vue
@@ -1,9 +1,10 @@
 <template>
 
   <Swipper/>
-
+  <CSwiper/>
 </template>
 <script setup lang="ts">
 import CBanner from "@/components/Sections/CBanner.vue";
 import Swipper from "@/components/swiperr/swipper.vue";
+import CSwiper from "@/components/CSwiper.vue";
 </script>
