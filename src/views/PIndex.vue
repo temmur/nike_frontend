cea1567a24a33aa4ba11cc259c2905c3b48815e8
temmur <143727496+temmur@users.noreply.github.com>
2025-08-30 14:09:58 +0500

    added tranlation to main

--- src/views/PIndex.vue
+++ src/views/PIndex.vue
@@ -9,7 +9,6 @@
 import CSwipper from '@/components/swiperr/swipper.vue'
 
 import CPlayGround from "@/components/sections/CPlayGround.vue";
-import CPlayGround from "@/components/Sections/CPlayGround.vue";
 import CBanner from "@/components/Sections/CBanner.vue";
 import CSwiper from "@/components/CSwiper.vue";
 </script>
