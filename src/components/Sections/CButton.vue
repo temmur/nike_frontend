a7ffab13173ed4abc13a8793b02693428ce2641b
Bob1rjon <bbbboburcik@gmail.com>
2025-09-04 14:54:39 +0500

    breadcrumb and corusel component created

---
+++ src/components/Sections/CButton.vue
@@ -0,0 +1,42 @@
+<template>
+  <button
+      class="flex items-center rounded-lg w-fit flex-center cursor-pointer"
+      :class="[variants[variant], sizes[size], { 'pointer-events-none opacity-50': disabled }, btnClass]"
+      v-bind="{ disabled }"
+  >
+    <slot name="prefix"></slot>
+    {{ text }}
+    <slot name="suffix"></slot>
+  </button>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  text?: string
+
+  disabled?: boolean
+  loading?: boolean
+  variant?: 'light' | 'dark'
+  size?: 'normal' | 'extra'
+
+}
+
+withDefaults(defineProps<Props>(), {
+  text: 'Button',
+  btnClass: '',
+  disabled: false,
+  loading: false,
+  variant: 'light',
+  iconName: ''
+})
+
+// Light & Dark variantlar uchun style
+const variants = {
+  light: 'rounded-full px-4 py-1.5 bg-white text-black font-medium hover:bg-gray-100 transition-colors',
+  dark: 'rounded-[14px] py-[10px] px-[20px] bg-black text-white border hover:bg-gray-800 active:scale-95 transition-all shadow'
+}
+// Size opsiyalari
+const sizes = {
+
+}
+</script>
--- src/views/PIndex.vue
+++ src/views/PIndex.vue
@@ -3,13 +3,17 @@
   <div>
     <CSwipper/>
     <CSwiper/>
+    <CShotsport/>
+    <CKorusel/>
   </div>
 </template>
 <script setup lang="ts">
 import CSwipper from '@/components/swiperr/swipper.vue'
 
 import CPlayGround from "@/components/sections/CPlayGround.vue";
-import CBanner from "@/components/Sections/CBanner.vue";
+import CKorusel from "@/components/sections/CKorusel.vue";
 import CSwiper from "@/components/CSwiper.vue";
+import CShotsport from "@/components/sections/CShotsport.vue";
+import breadcrumb from '@/components/Common/breadcrumb.vue'
 </script>
 
